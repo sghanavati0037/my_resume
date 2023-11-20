06e0b4e1affb880b5cd2f7a2a834c0eabe986fa7
sghanavati0037 <s_ghanavati@yahoo.com>
2023-11-20 09:48:40 +0330

    Add Requirements
    add requirements.txt

--- website/views.py
+++ website/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request,'website/index.html')
+    context = {'title':'Sara Ghanavati'}
+    return render(request,'website/index.html', context)
